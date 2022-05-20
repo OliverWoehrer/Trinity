3e3045d531d3a200b5183b04ed07cd968c7aad31
MCA Labor Account 3 <mclab3@ICT.TUWIEN.AC.AT>
2022-05-20 13:22:49 +0200

    standing pixels, poti used for engine speed

--- main.c
+++ main.c
@@ -19,7 +19,27 @@
 */
 #include "stm32f30x_conf.h"
 
-#define RES_BUFFER_LENGTH (20)
+#define RES_BUFFER_LENGTH 20
+#define TIM_PRE_SCALER 299
+#define TIME_SLOTS 2052
+#define IMG_HIGHT 12
+
+//unsigned int cntPerRound;
+unsigned int cntPerSlot;
+unsigned int slotState;
+unsigned int isUpdated = 1;
+
+void RCC_Config(void){
+	RCC_AHBPeriphClockCmd(RCC_AHBPeriph_GPIOA, ENABLE); 				// port A: NSS, UART
+	RCC_AHBPeriphClockCmd(RCC_AHBPeriph_GPIOB, ENABLE); 				// port B: SPI
+	RCC_AHBPeriphClockCmd(RCC_AHBPeriph_GPIOC, ENABLE); 				// port C: ADC
+	RCC_APB1PeriphClockCmd(RCC_APB1Periph_TIM2, ENABLE);  				// enable TIM2 (from APB1 clock)
+	RCC_APB1PeriphClockCmd(RCC_APB1Periph_USART2, ENABLE); 				// enable USART2 (from APB1 clock)
+	RCC_APB2PeriphClockCmd(RCC_APB2Periph_SPI1, ENABLE); 				// enable SPI (from APB2 clock)
+	RCC_APB1PeriphClockCmd(RCC_APB1Periph_TIM3, ENABLE); 				// enable TIM3 (from APB2 clock)
+	RCC_AHBPeriphClockCmd(RCC_AHBPeriph_ADC12, ENABLE); 				// enable ADC12 (from AHB clock)
+	RCC_ADCCLKConfig(RCC_ADC12PLLCLK_OFF); 								// use AHB clock signal for converting
+}
 
 void GPIO_Config(void){
 	GPIO_InitTypeDef GPIO_InitStructure;
@@ -28,14 +48,6 @@ void GPIO_Config(void){
 	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_OUT;						// pin mode "output"
 	GPIO_InitStructure.GPIO_OType = GPIO_OType_PP;						// output type "push-pull"
 	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_1;  							// OR operator to config both pins
-	GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_NOPULL;					// no pull up/down resistor needed
-	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;					// standart, speed does not matter that much
-	GPIO_Init(GPIOB, &GPIO_InitStructure); 								// initialize port B
-
-	//<<< GPIO (Optical Sensor) >>>
-	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF;						// pin mode "alternate function"
-	GPIO_InitStructure.GPIO_OType = GPIO_OType_PP;						// output type "push-pull"
-	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_0;  							// 
 	GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_NOPULL;					// no pull up/down resistor needed
 	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;					// standart, speed does not matter that much
 	GPIO_Init(GPIOB, &GPIO_InitStructure); 								// initialize port B
@@ -64,10 +76,10 @@ void GPIO_Config(void){
 	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_15;  						// OR operator to config both pins
 	GPIO_Init(GPIOA, &GPIO_InitStructure); 								// initialize port A
 	
-	// Connect Pins to Alternate Function:
+	// SPI Pins to Alternate Function:
 	GPIO_PinAFConfig(GPIOB, GPIO_PinSource3, GPIO_AF_5); 				// -> Datasheet P.42, Table 14. "Alternate  Functions"
-  	GPIO_PinAFConfig(GPIOB, GPIO_PinSource4, GPIO_AF_5); 				// -> AF Num. 5 (PB3=SCLK, PB4=MISO, PB5=MOSI)
-  	GPIO_PinAFConfig(GPIOB, GPIO_PinSource5, GPIO_AF_5);
+	GPIO_PinAFConfig(GPIOB, GPIO_PinSource4, GPIO_AF_5); 				// -> AF Num. 5 (PB3=SCLK, PB4=MISO, PB5=MOSI)
+	GPIO_PinAFConfig(GPIOB, GPIO_PinSource5, GPIO_AF_5);
 	GPIO_PinAFConfig(GPIOA, GPIO_PinSource15, GPIO_AF_5);
 
 	//<<< GPIO (ADC) >>>
@@ -79,56 +91,14 @@ void GPIO_Config(void){
 	GPIO_Init(GPIOC, &GPIO_InitStructure); 	 						 	// initialize port C
 
 
-	//<<< GPIO (TIM1) >>>
-	GPIO_PinAFConfig(GPIOB, GPIO_PinSource0, GPIO_AF_6);
-}
-
-void RCC_Config(void){
-	RCC_AHBPeriphClockCmd(RCC_AHBPeriph_GPIOA, ENABLE); 				// port A: NSS, UART
-	RCC_AHBPeriphClockCmd(RCC_AHBPeriph_GPIOB, ENABLE); 				// port B: SPI
-	RCC_AHBPeriphClockCmd(RCC_AHBPeriph_GPIOC, ENABLE); 				// port C: ADC
-	RCC_APB1PeriphClockCmd(RCC_APB1Periph_TIM2, ENABLE);  				// enable TIM2 (from APB1 clock)
-	RCC_APB1PeriphClockCmd(RCC_APB1Periph_USART2, ENABLE); 				// enable USART2 (from APB1 clock)
-	RCC_APB2PeriphClockCmd(RCC_APB2Periph_SPI1, ENABLE); 				// enable SPI (from APB2 clock)
-	RCC_APB2PeriphClockCmd(RCC_APB2Periph_TIM1, ENABLE); 				// enable TIM1 (from APB2 clock)
-	RCC_AHBPeriphClockCmd(RCC_AHBPeriph_ADC12, ENABLE); 				// enable ADC12 (from AHB clock)
-	RCC_ADCCLKConfig(RCC_ADC12PLLCLK_OFF); 								// use AHB clock signal for converting
-}
-
-void NVIC_Config(void){
-	NVIC_InitTypeDef NVIC_InitStructure;
-
-	// Set Number of Bits fot Priority Groups:
-	NVIC_PriorityGroupConfig(NVIC_PriorityGroup_2);
-	
-	// UART2:
-	NVIC_InitStructure.NVIC_IRQChannel = USART2_IRQn;				// -> Reference Manual P.195, Table 35: "Vector Table"
-	NVIC_InitStructure.NVIC_IRQChannelCmd = ENABLE;					// enable channel for interrupt
-	NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority = 3; // lowest priority (highest value)
-	NVIC_InitStructure.NVIC_IRQChannelSubPriority = 0;			// set sub-priority to zero
-	NVIC_Init(&NVIC_InitStructure);
-
-	// TIM2:
-	NVIC_InitStructure.NVIC_IRQChannel = TIM2_IRQn;
-	NVIC_InitStructure.NVIC_IRQChannelCmd = ENABLE;
-	NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority = 1;
-	NVIC_InitStructure.NVIC_IRQChannelSubPriority = 1;
-	NVIC_Init(&NVIC_InitStructure);
-  
-	// SPI1:
-	NVIC_InitStructure.NVIC_IRQChannel = SPI1_IRQn;
-	NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority = 0;
-	NVIC_InitStructure.NVIC_IRQChannelSubPriority = 0;
-	NVIC_InitStructure.NVIC_IRQChannelCmd = ENABLE;
-	NVIC_Init(&NVIC_InitStructure);
-
 	// TIM1:
-	NVIC_InitStructure.NVIC_IRQChannel = TIM1_CC_IRQn;
-	NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority = 1;
-	NVIC_InitStructure.NVIC_IRQChannelSubPriority = 1;
-	NVIC_InitStructure.NVIC_IRQChannelCmd = ENABLE;
-	NVIC_Init(&NVIC_InitStructure);
-
+	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF;						// pin mode "alternate function"
+	GPIO_InitStructure.GPIO_OType = GPIO_OType_PP;						// output type "push-pull"
+	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_0;  							// 
+	GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_NOPULL;					// no pull up/down resistor needed
+	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;					// standart, speed does not matter that much
+	GPIO_Init(GPIOB, &GPIO_InitStructure); 								// initialize port B
+	GPIO_PinAFConfig(GPIOB, GPIO_PinSource0, GPIO_AF_2); // alternate function
 }
 
 void TIM_Config(void){
@@ -138,25 +108,24 @@ void TIM_Config(void){
 	// Initialize Timer 2:
 	TIM_TimeBaseStructInit(&TIM_TimeBaseInitStructure);
 	TIM_TimeBaseInitStructure.TIM_Prescaler = 63999; // 64 MHz clock devided by 7999+1 -> 1kHz timer frequency
-	TIM_TimeBaseInitStructure.TIM_Period = 999; // 9999+1 counts -> 10 sec
+	TIM_TimeBaseInitStructure.TIM_Period = 999; // 999+1 counts -> 1 sec
 	TIM_TimeBaseInit(TIM2,&TIM_TimeBaseInitStructure);
-	
-	// Enable Interrupt on Update (Timer 2):
-	TIM_ITConfig(TIM2, TIM_IT_Update, ENABLE);
+	TIM_ITConfig(TIM2, TIM_IT_Update, ENABLE); // enable Interrupt on update
 
-	// Init Timer 1:
-	TIM_ICInitStructure.TIM_Channel = TIM_Channel_2;
+	// Initialize Timer 3:
+	TIM_TimeBaseStructInit(&TIM_TimeBaseInitStructure);
+	TIM_TimeBaseInitStructure.TIM_Prescaler = TIM_PRE_SCALER; // 64 MHz clock devided by 399+1 -> 160kHz timer frequency
+	TIM_TimeBaseInit(TIM3,&TIM_TimeBaseInitStructure);
+
+	TIM_ICInitStructure.TIM_Channel = TIM_Channel_3;
 	TIM_ICInitStructure.TIM_ICPolarity = TIM_ICPolarity_Rising;
 	TIM_ICInitStructure.TIM_ICSelection = TIM_ICSelection_DirectTI;
 	TIM_ICInitStructure.TIM_ICPrescaler = TIM_ICPSC_DIV1;
 	TIM_ICInitStructure.TIM_ICFilter = 0x0;
-	TIM_ICInit(TIM2, &TIM_ICInitStructure);
+	TIM_ICInit(TIM3, &TIM_ICInitStructure);
   
-	/* TIM enable counter */
-	TIM_Cmd(TIM2, ENABLE);
-
-	/* Enable the CC2 Interrupt Request */
-	TIM_ITConfig(TIM2, TIM_IT_CC2, ENABLE);
+	TIM_Cmd(TIM3, ENABLE); // enable counter
+	TIM_ITConfig(TIM3, TIM_IT_CC3, ENABLE); // Enable the CC2 Interrupt Request 
 }
 
 void USART_Config(void){
@@ -240,6 +209,42 @@ void ADC_Config(void){
 	ADC_StartConversion(ADC1);
 }
 
+void NVIC_Config(void){
+	NVIC_InitTypeDef NVIC_InitStructure;
+
+	// Set Number of Bits fot Priority Groups:
+	NVIC_PriorityGroupConfig(NVIC_PriorityGroup_2);
+	
+	// UART2:
+	NVIC_InitStructure.NVIC_IRQChannel = USART2_IRQn;				// -> Reference Manual P.195, Table 35: "Vector Table"
+	NVIC_InitStructure.NVIC_IRQChannelCmd = ENABLE;					// enable channel for interrupt
+	NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority = 3; // lowest priority (highest value)
+	NVIC_InitStructure.NVIC_IRQChannelSubPriority = 0;			// set sub-priority to zero
+	NVIC_Init(&NVIC_InitStructure);
+
+	// TIM2:
+	NVIC_InitStructure.NVIC_IRQChannel = TIM2_IRQn;
+	NVIC_InitStructure.NVIC_IRQChannelCmd = ENABLE;
+	NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority = 0;
+	NVIC_InitStructure.NVIC_IRQChannelSubPriority = 0;
+	NVIC_Init(&NVIC_InitStructure);
+  
+	// SPI1:
+	NVIC_InitStructure.NVIC_IRQChannel = SPI1_IRQn;
+	NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority = 0;
+	NVIC_InitStructure.NVIC_IRQChannelSubPriority = 1;
+	NVIC_InitStructure.NVIC_IRQChannelCmd = ENABLE;
+	NVIC_Init(&NVIC_InitStructure);
+
+	// TIM1:
+	NVIC_InitStructure.NVIC_IRQChannel = TIM3_IRQn;
+	NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority = 0;
+	NVIC_InitStructure.NVIC_IRQChannelSubPriority = 0;
+	NVIC_InitStructure.NVIC_IRQChannelCmd = ENABLE;
+	NVIC_Init(&NVIC_InitStructure);
+
+}
+
 void UARTPrint( char string[], int xvalue){
 	unsigned int i = 0;
 	while(string[i] != '\0') {
@@ -256,17 +261,17 @@ void UARTPrint( char string[], int xvalue){
 void setEngineSpeed(int speed) {
 	GPIO_ResetBits(GPIOB, GPIO_Pin_1);
 	SPI_SendData8(SPI1, 0x70|speed); // ((speed-0x36)<<2)
-	SPI_I2S_ITConfig(SPI1, SPI_I2S_IT_TXE, ENABLE); // enable the Tx buffer empty interrupt
 	while (SPI_GetTransmissionFIFOStatus(SPI1) != SPI_TransmissionFIFOStatus_Empty) {}; // Waiting until TX FIFO is empty
 	while(SPI_I2S_GetFlagStatus(SPI1, SPI_I2S_FLAG_BSY) == SET) {}; // Wait busy flag
+	SPI_I2S_ITConfig(SPI1, SPI_I2S_IT_TXE, ENABLE); // enable the Tx buffer empty interrupt
 }
 
 void setDemoMode(int mode) {
 	GPIO_ResetBits(GPIOB, GPIO_Pin_1);
 	SPI_SendData8(SPI1, 0x90|mode); //(recvd-0x30)
-	SPI_I2S_ITConfig(SPI1, SPI_I2S_IT_TXE, ENABLE); // enable the Tx buffer empty interrupt
 	while (SPI_GetTransmissionFIFOStatus(SPI1) != SPI_TransmissionFIFOStatus_Empty) {}; // Waiting until TX FIFO is empty
 	while(SPI_I2S_GetFlagStatus(SPI1, SPI_I2S_FLAG_BSY) == SET) {}; // Wait busy flag
+	SPI_I2S_ITConfig(SPI1, SPI_I2S_IT_TXE, ENABLE); // enable the Tx buffer empty interrupt
 }
 
 void setLeds(char leds1, char leds2, char leds3, char row) {
@@ -275,10 +280,11 @@ void setLeds(char leds1, char leds2, char leds3, char row) {
 	SPI_SendData8(SPI1, leds2);
 	SPI_SendData8(SPI1, leds3);
 	SPI_SendData8(SPI1, row);
-	SPI_I2S_ITConfig(SPI1, SPI_I2S_IT_TXE, ENABLE); // enable the Tx buffer empty interrupt
 	while (SPI_GetTransmissionFIFOStatus(SPI1) != SPI_TransmissionFIFOStatus_Empty) {}; // Waiting until TX FIFO is empty
 	while(SPI_I2S_GetFlagStatus(SPI1, SPI_I2S_FLAG_BSY) == SET) {}; // Wait busy flag
+	SPI_I2S_ITConfig(SPI1, SPI_I2S_IT_TXE, ENABLE); // enable the Tx buffer empty interrupt
 }
+
 
 int main(void) {
 	//<<< Config >>>
@@ -292,8 +298,51 @@ int main(void) {
 	
 	NVIC_Config();
 	
-	while(1)
-	{}
+	// Update Timer 2 Prescaler to match Timer 3:
+	TIM_TimeBaseInitTypeDef TIM_TimeBaseInitStructure;
+	TIM_TimeBaseStructInit(&TIM_TimeBaseInitStructure);
+	TIM_TimeBaseInitStructure.TIM_Prescaler = TIM_PRE_SCALER; // 64 MHz clock devided by 7999+1 -> 1kHz timer frequency
+	TIM_TimeBaseInit(TIM2,&TIM_TimeBaseInitStructure);
+	TIM_ITConfig(TIM2, TIM_IT_Update, ENABLE); // enable Interrupt on update
+	TIM_Cmd(TIM2, ENABLE);
+	
+	while(1) {
+		// Update Timer 2 run time:
+		TIM_SetAutoreload(TIM2, cntPerSlot);
+		
+		if(isUpdated) {
+			isUpdated = 0;
+			int adcRaw = ADC_GetConversionValue(ADC1); // 0...4095
+			
+			setEngineSpeed( (adcRaw*15)/4095 );
+			setLeds(0xFF, 0xFF, 0xFF, 0x10);
+			/*switch(slotState) {
+				case 0:
+					setLeds(0xC0, 0xC0, 0xC1, 0x10);
+					break;
+				case 2:
+					setLeds(0xC0, 0xC0, 0xC1, 0x12);
+					break;
+				case 4:
+					setLeds(0xC0, 0xC0, 0xC1, 0x14);
+					break;
+				case 6:
+					setLeds(0xC0, 0xC0, 0xC1, 0x10);
+					break;
+				case 8:
+					setLeds(0xC0, 0xC0, 0xC1, 0x12);
+					break;
+				case 10:
+					setLeds(0xC0, 0xC0, 0xC1, 0x14);
+					break;
+				default:
+					break;
+			}*/
+			
+			
+		}
+		
+	}
 
 }
 
@@ -323,22 +372,24 @@ void USART2_IRQHandler() {
 }
 
 void TIM2_IRQHandler() {
-	/* send adc value in loop:
-	int adcRaw = ADC_GetConversionValue(ADC1);
-	double adcValue = ((double)adcRaw)/(4095) * 3.3; // 12 bit = 4096 steps, Vdd=3.3V
-
-	while(USART_GetFlagStatus(USART2, USART_FLAG_TXE)!=SET) {};
-	int adcChar = (int) adcValue;
-	USART_SendData(USART2, 0x30 | adcChar);
-	*/
-	
+	slotState = slotState + 1;
+	if(slotState >= IMG_HIGHT) {
+		slotState = 0;
+	}
+	isUpdated = 1;
 	TIM_ClearITPendingBit(TIM2,TIM_IT_Update); // clear pending bit manually
 }
 
 void SPI1_IRQHandler() {
 	SPI_I2S_ITConfig(SPI1, SPI_I2S_IT_TXE, DISABLE);
 	SPI_I2S_ClearFlag(SPI1, SPI_I2S_FLAG_TXE);
-	UARTPrint("SPI Sent!\r\n",0x30);
+	//UARTPrint("SPI Sent!\r\n",0x30);
 	GPIO_SetBits(GPIOB, GPIO_Pin_1);
+}
+
+void TIM3_IRQHandler() {
+	TIM_SetCounter(TIM3, 0);
+	TIM_SetCounter(TIM2, 0);
+	cntPerSlot = TIM_GetCapture3(TIM3) / 6;
 }
 
